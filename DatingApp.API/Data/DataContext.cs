2a4ddafa8b478c36f730de58f7dd81d4301cb2d5
Robert Tumilty <Robert.Tumilty@ijyi.com>
2020-05-13 09:44:18 +0100

    Initial Commit

---
+++ DatingApp.API/Data/DataContext.cs
@@ -0,0 +1,13 @@
+using DatingApp.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DatingApp.API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base (options){}
+
+        // Table Names:
+        public DbSet<Value> Values { get; set; }
+    }
+}

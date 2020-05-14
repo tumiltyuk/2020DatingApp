a0bee160eb137132537e3fb9abd0270b03c5dfbf
Robert Tumilty <Robert.Tumilty@ijyi.com>
2020-05-14 13:06:39 +0100

    End of Section 3

---
+++ DatingApp.API/Controllers/AuthController.cs
@@ -0,0 +1,89 @@
+using System;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using DatingApp.API.Data;
+using DatingApp.API.Dtos;
+using DatingApp.API.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+
+namespace DatingApp.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly IAuthRepository _repo;
+        private readonly IConfiguration _config;
+        public AuthController(IAuthRepository repo, IConfiguration config)
+        {
+            _config = config;
+            _repo = repo;
+        }
+
+        [HttpPost("Register")]
+        public async Task<IActionResult> Register(UserForRegisterDto userForRegisterDto)
+        {
+            userForRegisterDto.Username = userForRegisterDto.Username.ToLower();
+
+            if (await _repo.UserExists(userForRegisterDto.Username))
+                return BadRequest("User already exists");
+
+            var userToCreate = new User
+            {
+                Username = userForRegisterDto.Username
+            };
+            var createdUser = await _repo.Register(userToCreate, userForRegisterDto.Password);
+
+            return StatusCode(201); // placeholder
+        }
+
+        [HttpPost("Login")]
+        public async Task<IActionResult> Login(UserForLoginDto userForLoginDto)
+        {
+            var userFromRepo = await _repo.Login(userForLoginDto.Username.ToLower(), userForLoginDto.Password);
+
+            if (userFromRepo == null)
+                return Unauthorized();
+
+            //Build Token to return to user (Token to contain "Claims": Username and UserId)
+            var claims = new[]
+            {   // Claim is a key value pair:  Claim Type & Value
+                new Claim(ClaimTypes.NameIdentifier, userFromRepo.Id.ToString()),   // User Id
+                new Claim(ClaimTypes.Name, userFromRepo.Username)                   // Username
+            };
+
+            // Generate "Key" to sign our token - (Hashed)  - Used for Validation check when future calls are made back from the client
+            //[Key is stored in our appsettings.json - value in here should be at least 12 char long and be a complicated string or random gen text]
+            var key = new SymmetricSecurityKey(Encoding.UTF8
+                .GetBytes(_config.GetSection("AppSettings:Token").Value));
+
+            // Generate Signin Credentials - takes security Key and algorithms used to hash our Key 
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
+
+            // Generate Token Discriptor that holds the token properties & claims
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(claims),
+                Expires = DateTime.Now.AddDays(0.5),
+                SigningCredentials = creds
+            };
+
+            // Generate Token Handler for creating a Token and passing in the Token Descriptor
+            var tokenHandler = new JwtSecurityTokenHandler();
+
+            // Generate Token
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+
+            // Return token as an object to the Client
+            return Ok(new {
+                token = tokenHandler.WriteToken(token)
+            });
+
+        }
+
+    }
+}
--- DatingApp.API/Data/DataContext.cs
+++ DatingApp.API/Data/DataContext.cs
@@ -9,5 +9,6 @@ namespace DatingApp.API.Data
 
         // Table Names:
         public DbSet<Value> Values { get; set; }
+        public DbSet<User> Users { get; set; }
     }
 }
